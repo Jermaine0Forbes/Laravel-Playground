d1769db9656c557e888baaf6bcec6a64be703088
Jermaine Forbes <jermaine0forbes@gmail.com>
2026-09-24 11:39:47 -0400

    updating the homepage to webapp3

--- webapp3/resources/js/pages/Welcome.vue
+++ webapp3/resources/js/pages/Welcome.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
-import { dashboard, login } from '@/routes';
-import { register } from '@/routes';
-import {ref} from "vue";
+import { Head, Link } from "@inertiajs/vue3";
+import { dashboard, login } from "@/routes";
+import { register } from "@/routes";
+import { ref } from "vue";
+import NavBarDefault from "@/components/NavBarDefault.vue";
 
 const count = ref(0);
 
@@ -10,54 +11,32 @@ const increment = () => count.value++;
 </script>
 
 <template>
-    <Head title="Welcome">
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
+  <Head title="Welcome">
+    <link rel="preconnect" href="https://rsms.me/" />
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+  </Head>
+  <div
+    class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
+  >
+    <NavBarDefault />
     <div
-        class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
+      class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
     >
-        <header
-            class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl"
-        >
-            <nav class="flex items-center justify-end gap-4">
-                <Link
-                    v-if="$page.props.auth.user"
-                    :href="dashboard()"
-                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                >
-                    Dashboard
-                </Link>
-                <template v-else>
-                    <Link
-                        :href="login()"
-                        class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                    >
-                        Log in
-                    </Link>
-                    <Link
-                        :href="register()"
-                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                    >
-                        Register
-                    </Link>
-                </template>
-            </nav>
-        </header>
-        <div
-            class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
-        >
-            <main
-                class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row"
-            >
-            <h1 class="text-xl">Testing out vue</h1>
-
-            <h2>Count</h2>
-            {{count}}
-
-
-            </main>
-        </div>
-        <div class="hidden h-14.5 lg:block"></div>
+      <main
+        class="flex w-full max-w-[335px] flex-col overflow-hidden rounded-lg lg:max-w-4xl"
+      >
+        <h1 class="text-xl mb-6">Testing out vue</h1>
+        <section>
+          <h2>Count</h2>
+          <button @click="increment"class="bg-teal-400 block p-2 border text-white">increment count</button>
+          {{ count }}
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
     </div>
+    <div class="hidden h-14.5 lg:block"></div>
+  </div>
 </template>
